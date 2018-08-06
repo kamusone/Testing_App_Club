063d296ad13c786017d63576827ac4fd55ce69c1
kamusone <40009107+kamusone@users.noreply.github.com>
2018-08-07 01:19:25 +0200

    Update static_pages_controller_test.rb

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,12 +1,20 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
-    get static_pages_home_url
-    assert_response :success
-  end
-  test "should get espace" do
-    get static_pages_home_url
-    assert_response :success
-  end
+  # test "login and check links" do
+  # 		get "/login"
+  # 		assert_response :success
+  #
+  # 		fill_in 'email', with: 'diego@gmail.com'
+  # 		fill_in 'espace', with: 'test'
+  #
+  #   end
+  # test "should get home" do
+  #   get static_pages_home_url
+  #   assert_response :success
+  # end
+  # test "should get espace" do
+  #   get static_pages_home_url
+  #   assert_response :success
+  # end
 end
